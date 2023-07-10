68ace2812443122c059071adfb47f367b4392007
suveshmoza <anmolmoza2@gmail.com>
2023-07-10 22:47:23 +0530

    added docker file

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+app.get('/', (req, res) => {
+	res.send('Hello there 👋 from Docker container');
+});
+
+app.listen(PORT, HOST, () => {
+	console.log(`Running on http://${HOST}:/${PORT}`);
+});
